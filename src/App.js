46b063d8050763ec2005b75576c1933675699926
Laxman <laxman.gupta1006@gmail.com>
2020-12-03 21:05:56 +0530

    Game History added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React,{useState} from "react";
-import Board from "./components/Board";
-import "./style/root.css"
-import calculateWinner from "./winner";
-const App=() => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+import React, { useState } from 'react';
+import Board from './components/Board';
+import './style/root.css';
+import calculateWinner from './winner';
+const App = () => {
   const [xturn, setXturn] = useState(
     [true, false][Math.floor(Math.random() * (1 - 0 + 1)) + 0]
   );
-
-  const winner = calculateWinner(board)
-  const message = winner ? `Winner is ${winner}` : `Playing for ${xturn ? 'X':'0'}`
+  const [history, setHistory] = useState([
+    {
+      board: Array(9).fill(null),
+      xturn: xturn,
+    },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
+  const winner = calculateWinner(current.board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Playing for ${xturn ? 'X' : '0'}`;
   const handleclick = position => {
-    if(board[position] || winner){
+    if (current.board[position] || winner) {
       return;
     }
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (square == null) {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+      const newBoard = last.board.map((square, pos) => {
+        if (square === null) {
           if (pos === position) {
-            let turn = xturn ? 'X' : '0';
+            let turn = last.xturn ? 'X' : '0';
             return turn;
           }
         }
         return square;
       });
+      return prev.concat({ board: newBoard, xturn: !last.xturn });
     });
-    setXturn(!xturn);
+    setCurrentMove(prev=>prev+1);
   };
-
-  return(
-<div className="app">
-  <h1>TIC TAC TOE</h1>
-  <h3>{message}</h3>
-  <Board
-  board ={board}
-  handleclick={handleclick}
-  />
-</div>)
+  console.log(history)
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h3>{message}</h3>
+      <Board board={current.board} handleclick={handleclick} />
+    </div>
+  );
 };
-export default App
+export default App;
